b8babe6105d98ce3d9dca819e5ab6dfe2b31ecf7
Brian Burt <beeburrt@outlook.com>
2022-12-27 21:21:58 -0800

    Add font, final styles

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>Monsters Rolodex</h1>
+        <h1 className="app-title">Monsters Rolodex</h1>
         <Searchbox onChangeHandler={onSearchChange} />
         <CardList monsters={filteredMonsters} />
       </div>
